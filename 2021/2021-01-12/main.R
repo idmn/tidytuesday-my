0e91e5f617fb33a6021ee2e8ef9396d4abc19b69
idmn <ya.domin@gmail.com>
2021-01-15 03:32:08 +0200

    upd

--- 2021/2021-01-12/main.R
+++ 2021/2021-01-12/main.R
@@ -40,6 +40,11 @@ df <-
   mutate(materials_lst = str_split(materials, "(, (and )?| and )"))
 
 
+# 
+df %>% 
+  count(year, sort = TRUE)
+
+
 #
 tmp <- 
   df %>% 
@@ -51,7 +56,7 @@ tmp <-
 #
 df_mat <- 
   df %>% 
-  select(yo, materials_lst) %>% 
+  select(yo, year, materials_lst) %>% 
   unnest(materials_lst) %>% 
   rename(material = materials_lst) %>% 
   mutate(material = str_to_lower(material)) %>% 
@@ -59,7 +64,10 @@ df_mat <-
     material == "waercolour" ~ "watercolour",
     TRUE ~ material
   )) %>% 
-  mutate(mat = fct_lump_min(material, 1000))
+  # mutate(mat = fct_lump_min(material, 1000) %>% fct_infreq())
+  mutate(mat = fct_lump_min(material, 3000) %>%
+           fct_infreq() %>% 
+           fct_relevel("Other", after = Inf))
 
 
 
@@ -67,14 +75,64 @@ df_mat %>%
   count(material, sort = TRUE)
 
 df_mat %>% 
+  mutate(mat = fct_rev(mat)) %>% 
   count(yo, mat, .drop = FALSE) %>% 
-  #filter(mat != "graphite") %>% 
+  # filter(mat != "graphite") %>% 
+  # filter(mat == "watercolour") %>% 
   ggplot(aes(yo, n, fill = mat)) +
-  geom_area() +
-  theme_minimal()
+  geom_area(position = "stack") +
+  #facet_wrap(~mat, ncol = 1, scales = "free_y") +
+  scale_fill_manual(values = c(
+    "graphite" = "#f94144",
+    "watercolour" = "#f3722c",
+    "gouache" = "#f9c74f",
+    "ink" = "#90be6d",
+    "chalk" = "#43aa8b",
+    "pen" = "#4d908e",
+    "Other" = "#277da1"
+  )) +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "gray30"),
+    panel.grid.major = element_line(color = "gray70"),
+    panel.grid.minor = element_blank(),
+    axis.text = element_text(color = "grey80")
+  )
 
+# colors <- c("#f3722c","#f8961e","#f9c74f","#90be6d","#43aa8b","#4d908e","#277da1")
+colors <- c("f3722c","43aa8b","f8961e","f9c74f","277da1","90be6d","4d908e")
+colors <- paste0("#", colors)
+names(colors) <- rev(levels(df_mat$mat))
 
+colors <- c("9D8189","84A98C","577590")
+colors <- paste0("#", colors)
+names(colors) <- rev(levels(df_mat$mat))
+text_color <- "grey90"
 
+df_mat %>% 
+  mutate(mat = fct_rev(mat)) %>% 
+  ggplot(aes(year, fill = mat, color = mat)) +
+  geom_bar(width = 1) +
+  scale_fill_manual(values = colors) +
+  scale_color_manual(values = colors) +
+  xlab(NULL) +
+  ylab(NULL) +
+  ggtitle("J. M. W. Turner artworks on paper") +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "gray20"),
+    panel.grid.major = element_line(color = "gray31"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    axis.text = element_text(color = text_color),
+    legend.text = element_text(color = text_color),
+    legend.title = element_blank(),
+    plot.title = element_text(color = text_color)
+  )
+
+# need two axes here: one for the guy's age, one for year
+# additional marks for high years
+# what about counts? some works are counted twice
 
 
 # simple plot to explain how much of all works is by this guy
